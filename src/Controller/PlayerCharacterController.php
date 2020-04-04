156894f07081f22e16b7385cf38ac8d0520c0613
SinaBennhardt <sina.bennhardt@gmail.com>
2020-04-04 13:24:11 +0200

    made Characters, NSC and Content deletable

--- src/Controller/PlayerCharacterController.php
+++ src/Controller/PlayerCharacterController.php
@@ -7,8 +7,11 @@ use App\Entity\Headword;
 use App\Entity\PlayerCharacterInfo;
 use App\Entity\PlayerProperties;
 use App\Entity\User;
+use App\Form\DeleteCharacterType;
+use App\Form\DeleteNscType;
 use App\Form\PlayerCharacterType;
 use App\Form\PCPropertyType;
+use Doctrine\ORM\Mapping\Id;
 use phpDocumentor\Reflection\DocBlock\Tags\Author;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -18,6 +21,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PlayerCharacterController extends AbstractController
@@ -31,12 +35,17 @@ class PlayerCharacterController extends AbstractController
      * @var ValidatorInterface
      */
     private $validator;
+
+    /**
+     * @var RouterInterface
+     */
     private $router;
 
-    public function __construct(EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    public function __construct(EntityManagerInterface $entityManager, ValidatorInterface $validator, RouterInterface $router)
     {
         $this->entityManager = $entityManager;
         $this->validator = $validator;
+        $this->router = $router;
     }
 
     /**
@@ -101,14 +110,16 @@ class PlayerCharacterController extends AbstractController
      * @Template()
      * @IsGranted("ROLE_USER")
      * @param Request $request
+     * @param id
      * @return array
      */
 
-    public function ChangeCharacterAction(Request $request)
+    public function ChangeCharacterAction(Request $request, $id)
     {
         $repository = $this->entityManager->getRepository(PlayerCharacterInfo::class);
-        $id = $request->attributes->get('id');
         $characterInfo = $repository->find($id);
+
+        $user = $this->getUser();
 
         $addPlayerCharacterForm = $this->createForm(PlayerCharacterType::class, $characterInfo);
         $addPlayerCharacterForm->handleRequest($request);
@@ -123,8 +134,51 @@ class PlayerCharacterController extends AbstractController
         }
 
         return ["addPlayerCharacterForm" => $addPlayerCharacterForm->createView(),
+            'id' => $id,
+            'user' => $user,
+            'characterInfo' => $characterInfo
         ];
     }
+
+    /**
+     * @Route("/character/delete/{id}", name="delete_character")
+     * @Template()
+     * @IsGranted("ROLE_USER")
+     * @param Request $request
+     * @param id
+     * @return array|RedirectResponse
+     */
+
+    public function deleteCharacterAction(Request $request, $id)
+    {
+        $repository = $this->entityManager->getRepository(PlayerCharacterInfo::class);
+        $character = $repository->find($id);
+
+        $deleteCharacterForm = $this->createForm(DeleteCharacterType::class);
+        $deleteCharacterForm->handleRequest($request);
+
+        if ($deleteCharacterForm->isSubmitted() && $deleteCharacterForm->isValid()) {
+
+            $repository = $this->entityManager->getRepository(PlayerProperties::class);
+            $characterProperties = $repository->find($id);
+
+            $this->addFlash('success',
+                sprintf('Der Held "%s" wurde erfolgreich gelöscht.', $character->getCharacterName()));
+
+            $this->entityManager->remove($character);
+            $this->entityManager->remove($characterProperties);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('character_sheet'));
+        }
+
+        return [
+            'deleteCharacterForm' => $deleteCharacterForm->createView(),
+            'id' => $id,
+            'character' => $character
+        ];
+    }
+
 
     /**
      * @Route("/nsc", name="all_nsc")
@@ -153,9 +207,9 @@ class PlayerCharacterController extends AbstractController
     /**
      * @Route("/nsc/create", name="new_nsc")
      * @Template()
-     * @IsGranted("ROLE_USER")
+     * @IsGranted("ROLE_ADMIN")
      * @param Request $request
-     * @return array
+     * @return array|RedirectResponse
      */
 
     public function CreateNewNscAction(Request $request)
@@ -180,10 +234,50 @@ class PlayerCharacterController extends AbstractController
             $this->entityManager->persist($characterInfo);
             $this->entityManager->persist($characterProperties);
             $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('all_nsc'));
         }
 
-
         return ["addPlayerCharacterForm" => $addPlayerCharacterForm->createView(),
+        ];
+    }
+
+    /**
+     * @Route("/nsc/delete/{id}", name="delete_nsc")
+     * @Template()
+     * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @param id
+     * @return array|RedirectResponse
+     */
+
+    public function deleteNscAction(Request $request, $id)
+    {
+        $repository = $this->entityManager->getRepository(PlayerCharacterInfo::class);
+        $nsc = $repository->find($id);
+
+        $deleteNscForm = $this->createForm(DeleteNscType::class);
+        $deleteNscForm->handleRequest($request);
+
+        if ($deleteNscForm->isSubmitted() && $deleteNscForm->isValid()) {
+
+            $repository = $this->entityManager->getRepository(PlayerProperties::class);
+            $nscProperties = $repository->find($id);
+
+            $this->addFlash('success',
+                sprintf('Der NSC "%s" wurde erfolgreich gelöscht.', $nsc->getCharacterName()));
+
+            $this->entityManager->remove($nsc);
+            $this->entityManager->remove($nscProperties);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('all_nsc'));
+        }
+
+        return [
+            'deleteNscForm' => $deleteNscForm->createView(),
+            'id' => $id,
+            'nsc' => $nsc
         ];
     }
 }
